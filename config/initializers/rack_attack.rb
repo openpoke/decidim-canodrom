d28c061c808580fe4495c3c8d267c59780296148
Ivan Vergés <ivan@pokecode.net>
2022-07-01 17:14:05 +0200

    skip logged users

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -7,13 +7,17 @@ if Rails.env.production? && Rails.application.secrets.rack_attack_skip.present?
   # Provided that trusted users use an HTTP request param named skip_rack_attack
   # with this you can perform apache benchmark test like this:
   # ab -n 2000 -c 20 'https://decidim.url/?skip_rack_attack=some-secret'
-  Rack::Attack.safelist("mark any authenticated access safe") do |request|
+  Rack::Attack.safelist("bypass active storage") do |request|
     # skip rails active storage routes
-    return true if request.path.start_with?("/rails/active_storage")
+    request.path.start_with?("/rails/active_storage")
+  end
 
+  Rack::Attack.safelist("bypass authenticated users") do |request|
     # skip logged users
-    return true if request.env['rack.session']["warden.user.user.key"].present?
+    request.env.dig("rack.session", "warden.user.user.key").present?
+  end
 
+  Rack::Attack.safelist("bypass with secret param") do |request|
     # Requests are allowed if the return value is truthy
     request.params["skip_rack_attack"] == Rails.application.secrets.rack_attack_skip
   end
