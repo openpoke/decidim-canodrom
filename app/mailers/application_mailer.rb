6f0f776e5b49a3a8e0d82bb70716b0d06aa1595b
Ivan Vergés <ivan@pokecode.net>
2022-07-27 12:38:30 +0200

    set different smtp on newsletter

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -3,4 +3,10 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "from@example.com"
   layout "mailer"
+
+  after_action :set_delivery_options
+
+  def set_delivery_options
+    byebug
+  end
 end
