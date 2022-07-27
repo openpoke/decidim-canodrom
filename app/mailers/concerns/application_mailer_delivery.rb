a8036ba9b12efa7df5246965598b0511ba1089d5
Ivan Vergés <ivan@pokecode.net>
2022-07-27 12:38:30 +0200

    set different smtp on newsletter

---
+++ app/mailers/concerns/application_mailer_delivery.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require "active_support/concern"
+
+module ApplicationMailerDelivery
+  extend ActiveSupport::Concern
+
+  included do
+	  after_action :set_delivery_options
+
+	  private
+
+	  def set_delivery_options
+	    return if Rails.application.secrets.broadcast_username.blank?
+	    return unless self.class.name == "Decidim::NewsletterMailer"
+
+      mail.delivery_method.settings.merge!(
+        address: broadcast_address,
+        user_name: broadcast_username,
+        password: broadcast_password
+      ) { |_k, o, v| v.presence || o }.compact_blank!
+	  end
+  end
+end
