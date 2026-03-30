af419431ca53db3da47f1c03b99c6901a13052a0
David Igón <david@pokecode.net>
2026-03-30 15:19:40 +0200

    update packages and shakapacker

--- app/controllers/decidim_controller.rb
+++ app/controllers/decidim_controller.rb
@@ -9,7 +9,8 @@ class DecidimController < ApplicationController
   private
 
   def set_raven_context
-    return unless Rails.application.secrets.sentry_enabled?
+    sentry_enabled = ENV.fetch("SENTRY_DSN", nil).present?
+    return unless sentry_enabled
 
     Raven.user_context({ id: try(:current_user).try(:id) }.merge(session))
     Raven.extra_context(params: params.to_unsafe_h, url: request.url)
